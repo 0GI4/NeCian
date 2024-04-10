5e08f18600d3497f2e4b3ca524a4b9c0cfabe5a7
0GI4 <pop9991@outlook.com>
2024-04-10 22:06:06 +0300

    editAlmostDone

--- components/pages/adminPage.jsx
+++ components/pages/adminPage.jsx
@@ -4,22 +4,51 @@ const AddAdv = require('../ui/addAdv');
 const AdvertismentCard = require('../ui/AdvertismentCard');
 
 module.exports = function adminPage({
-  advertisments, title, user, categories,
+  advertisments,
+  title,
+  user,
+  categories,
 }) {
   return (
     <Layout title={title} user={user}>
-      <AddAdv user={user}/* categories={categories} */ />
+      <AddAdv user={user} /* categories={categories} */ />
       <div className="listAdmin">
         {advertisments.map((el) => (
           <div
             key={el.id}
             className="card"
-            data-id={advertisments.id}
+            data-id={el.id}
             style={{ width: '18rem', margin: '20px' }}
           >
             <AdvertismentCard advertisment={el} />
-            { user && <button className="del">Удалить</button>}
-            { user && <button className="del upd">Изменить</button>}
+            {user && <button className="del">Удалить</button>}
+            {user && (
+              <button className="del upd updateBtn" data-id={el.id}>
+                Изменить
+              </button>
+            )}
+            {user && (
+              <form
+                className={`updateForm formUpdate${el.id} hidden`}
+                data-id={el.id}
+              >
+                <input
+                  type="text"
+                  name="description"
+                  placeholder="Описание"
+                  defaultValue={el.description}
+                />
+                <input
+                  type="text"
+                  name="price"
+                  placeholder="Цена"
+                  defaultValue={el.price}
+                />
+                <button type="submit" className="saveUpdate" data-id={el.id}>
+                  Сохранить
+                </button>
+              </form>
+            )}
           </div>
         ))}
       </div>
