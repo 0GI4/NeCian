b0f67dbe144cfab90f869fa0176d4437982c737c
0GI4 <pop9991@outlook.com>
2024-04-10 14:39:23 +0300

    Log/RegDone

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,6 +8,8 @@ module.exports = function Layout({ title, children, user }) {
       <head>
         <title>{title}</title>
         <link rel="stylesheet" href="/style/navbar.css" />
+        <link rel="stylesheet" href="/style/style.css" />
+        <script defer src="/scripts/authScripts.js" />
       </head>
       <body>
         <NavBar user={user} />
