1bf9d7b7434e0160caa68a10a0162bfb3bf08230
anastasiya <kesyan_anastasiya@mail.ru>
2024-04-10 15:53:49 +0300

    удалила лишний AdsList

--- components/pages/AdsList.jsx
+++ components/pages/AdsList.jsx
@@ -1,10 +1,12 @@
 const React = require("react");
 const Layout = require("../Layout");
 const AdvertismentCard = require("../ui/AdvertismentCard");
+const FilterHouse = require('./FilterHouse')
 
 module.exports = function AdsList({ title, user, advertisments }) {
   return (
     <Layout title={title} user={user}>
+      <FilterHouse/>
       <div className="advertismentList advertisment-container">
         {advertisments.map((el) => (
           <AdvertismentCard
